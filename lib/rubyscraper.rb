ca7ec2e648da4e2d94cf93146995109543fa137a
Nathan Owsiany <nowsiany@gmail.com>
2015-04-22 21:54:45 -0600

    basic scraping functionality

--- lib/rubyscraper.rb
+++ lib/rubyscraper.rb
@@ -1,17 +1,65 @@
-require 'rss'
+require 'capybara'
+require 'capybara/poltergeist'
 require 'rubyscraper/version'
+
+include Capybara::DSL
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
+
+Capybara.default_driver = :poltergeist
 
 class RubyScraper
   def self.call(endpoint)
-    terms = get_search_terms("search-terms.txt")
-    jobs_scraped = 0
-    jobs_saved = 0
-    terms.each do |term|
-      scraped, saved = pull_rss(term)
-      jobs_scraped += scraped
-      jobs_saved += saved
+    visit "http://careers.stackoverflow.com/jobs?searchTerm=ruby&sort=p"
+    start = Time.now
+    jobs = []
+
+    (1..2).to_a.each do |page|
+      visit "http://careers.stackoverflow.com/jobs?searchTerm=ruby&sort=p&pg=#{page}"
+
+      all(".listResults .-item").each do |listing|
+        position = listing.find("h3.-title a").text
+        url = listing.find("h3.-title a")["href"]
+        posting_date = listing.first("p._muted").text
+
+        jobs << {
+          position: position,
+          url: url,
+          posting_date: posting_date
+        }
+      end
+      puts "page #{page}"
     end
-    return jobs_scraped, jobs_saved
+
+    jobs.each_with_index do |job, i|
+      puts "job pull #{i}"
+      sleep 1
+      visit "http://careers.stackoverflow.com#{job[:url]}"
+      if has_css?("a.employer")
+        job[:company] = find("a.employer").text
+      end
+      if has_css?("span.location")
+        job[:location] = find("span.location").text
+      end
+      #job[:description] = first("div.description p")
+      description = all("div.description p").map do |p|
+        p.text
+      end
+      job[:description] = description.join("\n")
+      tags = all("div.tags a.post-tag").map do |tag|
+        tag.text
+      end
+      job[:tags] = tags
+    end
+
+    puts jobs.first
+    puts jobs.last
+    puts pages
+    elapsed = Time.now - start
+    puts elapsed
+    return jobs.length, 0
   end
 
   def self.pull_rss(term)
@@ -25,41 +73,6 @@ class RubyScraper
     return rss.items.count, saved
   end
 
-  # SAMPLE ITEM
-  # <item>
-  #   <guid isPermaLink="true">
-  #     http://careers.stackoverflow.com/jobs/84858/awesome-ruby-on-rails-engineer-veritrans-indonesia
-  #   </guid>
-  #   <link>
-  #     http://careers.stackoverflow.com/jobs/84858/awesome-ruby-on-rails-engineer-veritrans-indonesia
-  #   </link>
-  #   <category>
-  #     ruby
-  #   </category>
-  #   <category>
-  #     ruby-on-rails
-  #   </category>
-  #   <title>
-  #     Awesome Ruby on Rails Engineer at Veritrans Indonesia (Kota Jakarta Pusat, Indonesia)
-  #   </title>
-  #   <description>
-  #     &lt;p&gt;You could work for a startup with yet another recycled social product idea, no business model, 
-  #     naive executive teams and ten minutes of funding.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;Or you could become part 
-  #     of the band at Veritrans Indonesia, a company with a&amp;nbsp;&lt;em&gt;real&amp;nbsp;&lt;/em&gt;solid 
-  #     vision led by an industry rock star and play a key role in developing the most advanced internet payment 
-  #     gateway platform in the country.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;You&amp;nbsp;&lt;em&gt;will&lt;/em&gt;&amp;nbsp;
-  #     have plenty of&amp;nbsp;&lt;em&gt;opportunities&lt;/em&gt;&amp;nbsp;to sharpen&amp;nbsp;your&amp;nbsp;skills in
-  #     adding new features and going beyond.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;You&amp;nbsp;&lt;em&gt;will&lt;/em&gt;
-  #     &amp;nbsp;work closely with t
-  #   </description>
-  #   <pubDate>
-  #     Mon, 06 Apr 2015 12:37:22 Z
-  #   </pubDate>
-  #   <a10:updated>
-  #     2015-04-06T12:37:22Z
-  #   </a10:updated>
-  # </item>
-
   def self.send_to_server(rss_item)
     date = DateTime.strptime(rss_item.pubDate.to_s, '%a, %e %b %Y %H:%M:%S %z')
     # parse item contents
@@ -71,16 +84,5 @@ class RubyScraper
     # If item is created return 201
     # If succesful save return true, else false
     return true
-  end
-
-  def self.get_search_terms(filename)
-    file = File.expand_path("../assets/#{filename}", __FILE__)
-    f = File.open(file, "r")
-    terms = []
-    f.each_line do |line|
-      terms << line.chomp
-    end
-    f.close
-    terms
   end
 end
